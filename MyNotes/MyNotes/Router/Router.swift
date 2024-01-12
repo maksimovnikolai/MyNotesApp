448cc61bbe736a37cdb5ed999dfd7180b62c2cda
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-12 14:02:48 +0300

    add router (navigation)

---
+++ MyNotes/MyNotes/Router/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  MyNotes
+//
+//  Created by Nikolai Maksimov on 12.01.2024.
+//
+
+import UIKit
+
+protocol RouterMainProtocol {
+    var navigationController: UINavigationController? { get set }
+    var assemblyBuilder: AssemblyBuilderProtocol? { get set }
+}
+
+protocol RouterProtocol: RouterMainProtocol {
+    func initialViewController()
+}
+
+final class Router: RouterProtocol {
+
+    var navigationController: UINavigationController?
+    var assemblyBuilder: AssemblyBuilderProtocol?
+    
+    init(navigationController: UINavigationController, assemblyBuilder: AssemblyBuilderProtocol) {
+        self.navigationController = navigationController
+        self.assemblyBuilder = assemblyBuilder
+    }
+    
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let mainViewController = assemblyBuilder?.createMainModule() else { return }
+            navigationController.viewControllers = [mainViewController]
+        }
+    }
+}
