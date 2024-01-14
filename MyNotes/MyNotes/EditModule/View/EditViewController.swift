94746807568a7126c7ca2d2464b476312246b330
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-14 21:03:09 +0300

    add two methods - numberOfRowsInSection & cellForRowAt

--- MyNotes/MyNotes/EditModule/View/EditViewController.swift
+++ MyNotes/MyNotes/EditModule/View/EditViewController.swift
@@ -15,11 +15,37 @@ final class EditViewController: UITableViewController {
     //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        commonInit()
     }
 }
 
 //MARK: - EditViewControllerProtocol
 extension EditViewController: EditViewControllerProtocol {
     
+}
+
+//MARK: - Private methods
+private extension EditViewController {
+    
+    func commonInit() {
+        registerTableViewCell()
+    }
+    
+    private func registerTableViewCell() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+//MARK: - TableViewDataSource
+extension EditViewController {
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
 }
