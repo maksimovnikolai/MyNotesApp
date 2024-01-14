dd81c703b81a79faf3f8ea306530a8845d311e36
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-14 22:24:35 +0300

    create TypeCell with 2 labels

---
+++ MyNotes/MyNotes/EditModule/View/TypeCell.swift
@@ -0,0 +1,66 @@
+//
+//  TypeCell.swift
+//  MyNotes
+//
+//  Created by Nikolai Maksimov on 14.01.2024.
+//
+
+import UIKit
+
+final class TypeCell: UITableViewCell {
+    
+    static let identifier = "TypeCell"
+    
+    //MARK: Private properties
+    private lazy var currentTypeLabel = makeLabel(textAlignment: .left)
+    private lazy var chooseTypeLabel = makeLabel(textColor: .systemGray, textAlignment: .right)
+    
+    
+    //MARK: Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: Private methods
+private extension TypeCell {
+    
+    func commonInit() {
+        setupCurrentTypeLabelConstraints()
+        setupChooseTypeLabelConstraints()
+    }
+    
+    func makeLabel(textColor: UIColor? = .black, textAlignment: NSTextAlignment) -> UILabel {
+        let label = UILabel()
+        label.text = "label"
+        label.textColor = textColor
+        label.textAlignment = textAlignment
+        
+        return label
+    }
+    
+    func setupCurrentTypeLabelConstraints() {
+        addSubview(currentTypeLabel)
+        currentTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            currentTypeLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            currentTypeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+        ])
+        currentTypeLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    func setupChooseTypeLabelConstraints() {
+        addSubview(chooseTypeLabel)
+        chooseTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            chooseTypeLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            chooseTypeLabel.leadingAnchor.constraint(equalTo: currentTypeLabel.trailingAnchor),
+            chooseTypeLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+        ])
+    }
+}
