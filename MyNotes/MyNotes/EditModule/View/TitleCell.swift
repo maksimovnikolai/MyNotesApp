92e0c2aed35c662375a25a52dc82ceaeffff9942
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-14 21:54:08 +0300

    create TitleCell with textfield

---
+++ MyNotes/MyNotes/EditModule/View/TitleCell.swift
@@ -0,0 +1,50 @@
+//
+//  TitleCell.swift
+//  MyNotes
+//
+//  Created by Nikolai Maksimov on 14.01.2024.
+//
+
+import UIKit
+
+final class TitleCell: UITableViewCell {
+    
+    static let identifier = "TitleCell"
+    
+    //MARK: Private properties
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Введите задачу"
+        textField.borderStyle = .none
+        textField.font = .systemFont(ofSize: 17)
+        return textField
+    }()
+    
+    //MARK: Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: Private methods
+private extension TitleCell {
+    
+    func commonInit() {
+        setupTextFieldConstraints()
+    }
+    
+    func setupTextFieldConstraints() {
+        addSubview(titleTextField)
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleTextField.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            titleTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2),
+        ])
+    }
+}
